2fd0b7c9e5d5296715970de2b44d5a8966690913
vasconcelos-gabriel <vasconcelos.crvg@gmail.com>
2025-04-21 20:52:32 -0300

    ajuste na exibição do usuario na tela de entrevista

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -160,13 +160,9 @@ const Agent = ({
         </div>
         <div className="card-border">
           <div className="card-content">
-            <Image
-              src="/user-avatar.png"
-              alt="user avatar"
-              width={540}
-              height={540}
-              className="rounded-full object-cover size-[120px]"
-            />
+            <div className="flex items-center justify-center rounded-full bg-primary-200 text-dark-100 font-semibold text-5xl size-[120px]">
+              {userName?.charAt(0).toUpperCase()}
+            </div>
             <h3>{userName}</h3>
           </div>
         </div>
